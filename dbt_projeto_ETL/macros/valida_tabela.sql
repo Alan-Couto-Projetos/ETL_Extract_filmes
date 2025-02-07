e7340e008223c70d319b92b54d30c1237a7f4c58
Alan-Couto-Projetos <alancouto300@gmail.com>
2025-02-07 20:35:26 -0300

    first commit

---
+++ dbt_projeto_ETL/macros/valida_tabela.sql
@@ -0,0 +1,17 @@
+{% test valida_tabela(model, column_name) %}
+
+    {{ config(severity = 'warn') }}
+
+    with consulta_teste as (
+
+    select
+        count({{ column_name }}) AS coluna_teste
+
+    from {{ model }}
+
+    )
+    SELECT * 
+    FROM consulta_teste
+    WHERE coluna_teste = 0
+
+{% endtest %}
---
+++ dbt_projeto_ETL/models/example/tabela_filmes.sql
@@ -0,0 +1,14 @@
+
+{{ config(materialized='table') }}
+
+with source_data as (
+   
+
+   SELECT * FROM tabela_filmes
+
+)
+
+SELECT *
+FROM source_data
+
+
